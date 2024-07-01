110622d41c116bfee8593df215f327bdcb073818
Tushar Agarwal <76068393+tusharagarwal211@users.noreply.github.com>
2024-07-01 23:03:53 +0530

    Time: 0 ms (100.00%), Space: 42.3 MB (19.26%) - LeetHub

---
+++ 1550-three-consecutive-odds/1550-three-consecutive-odds.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean threeConsecutiveOdds(int[] arr) {
+        int cntOdd = 0;
+
+        for(int i = 0; i < arr.length; i++) {
+            if(arr[i]%2 != 0) {
+                cntOdd++;
+                if(cntOdd == 3) {
+                    return true;
+                }
+            }
+            else {
+                cntOdd = 0;
+            }
+        }
+
+        return false;
+    }
+}
