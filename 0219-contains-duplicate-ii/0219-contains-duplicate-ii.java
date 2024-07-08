e3df1ee758ab4ce957fc736d269169077e3204d1
Tushar Agarwal <76068393+tusharagarwal211@users.noreply.github.com>
2024-07-08 23:35:38 +0530

    Time: 18 ms (69.94%), Space: 57.8 MB (31.89%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -1,17 +1,21 @@
-class Solution {
+public class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-        
-        HashMap<Integer, Integer> hmap = new HashMap<>();
-            
-        for(int i = 0; i < nums.length; i++) {
-            if(hmap.containsKey(nums[i])) {
-                if(Math.abs(hmap.get(nums[i]) - i) <= k) {
+        int n = nums.length;
+
+        HashMap<Integer, Integer> numIndices = new HashMap<>();
+
+        for (int currentIndex = 0; currentIndex < n; ++currentIndex) {
+            int currentNum = nums[currentIndex];
+
+            if (numIndices.containsKey(currentNum)) {
+                if (currentIndex - numIndices.get(currentNum) <= k) {
                     return true;
                 }
             }
-            hmap.put(nums[i], i);
+
+            numIndices.put(currentNum, currentIndex);
         }
-        
+
         return false;
     }
 }
