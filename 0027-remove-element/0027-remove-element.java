f4d432d20311fee4397d6cc7bb995da7e56a9c17
Tushar Agarwal <76068393+tusharagarwal211@users.noreply.github.com>
2024-07-13 19:44:56 +0530

    Time: 1 ms (6.76%), Space: 41.8 MB (53.90%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int count = 0;
+        
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] == val) {
+                nums[i] = 101;
+                count++;
+            }
+        }
+        
+        Arrays.sort(nums);
+        
+        return nums.length-count;
+    }
+}
