a8ecedbdcaa6e55dfbd4ee98177229003a6d0664
Tushar Agarwal <76068393+tusharagarwal211@users.noreply.github.com>
2024-07-13 16:56:18 +0530

    Time: 18 ms (34.24%), Space: 55.1 MB (83.04%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,13 +1,13 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        
         if(nums.length == 1) {
             return false;
         }
         
-        HashSet<Integer> set = new HashSet<>();
-        
-        for(int num : nums) {
-            if(!set.add(num)) {
+        for(int i = 0; i < nums.length - 1; i++) {
+            if(nums[i] == nums[i + 1]) {
                 return true;
             }
         }
