12685f3b18a0f4431bdb9a3ddd18ee25979d9370
Tushar Agarwal <76068393+tusharagarwal211@users.noreply.github.com>
2024-07-14 15:57:30 +0530

    Time: 3 ms (88.70%), Space: 44.8 MB (14.14%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -2,33 +2,20 @@ class Solution {
     public boolean isAnagram(String s, String t) {
         int sLength = s.length();
         int tLength = t.length();
-        int value = 0;
         
         if(sLength != tLength) {
             return false;
         }
         
-        HashMap<Character, Integer> sHmap = new HashMap<>();
-        HashMap<Character, Integer> tHmap = new HashMap<>();
+        char[] sArr = s.toCharArray();
+        Arrays.sort(sArr);
+        String sSorted = new String(sArr);
         
-        for(int i = 0; i < sLength; i++) {
-            if(sHmap.containsKey(s.charAt(i))) {
-                value = sHmap.get(s.charAt(i)) + 1;
-                sHmap.put(s.charAt(i), value);
-            }
-            else {
-                sHmap.put(s.charAt(i), 1);
-            }
-            if(tHmap.containsKey(t.charAt(i))) {
-                value = tHmap.get(t.charAt(i)) + 1;
-                tHmap.put(t.charAt(i), value);
-            }
-            else {
-                tHmap.put(t.charAt(i), 1);
-            }
-        }
+        char[] tArr = t.toCharArray();
+        Arrays.sort(tArr);
+        String tSorted = new String(tArr);
         
-        boolean ans = sHmap.equals(tHmap);
+        boolean ans = sSorted.equals(tSorted);
         
         return ans;
     }
